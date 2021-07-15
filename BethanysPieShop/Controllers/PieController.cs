2cff00ff65128e982a45f837fb35c813718ad834
Ali <alicetin.dev@gmail.com>
2021-07-16 01:56:46 +0300

    first commit

---
+++ BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,32 @@
+﻿using BethanysPieShop.Models;
+using BethanysPieShop.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            //ViewBag.CurrentCategory = "Cheese cakes";
+            PiesListViewModel piesListViewModel = new PiesListViewModel();
+            piesListViewModel.Pies = _pieRepository.AllPies;
+
+            piesListViewModel.CurrentCategory = "Cheese cakes";
+            return View(piesListViewModel);
+        }
+    }
+}
---
+++ BethanysPieShop/Models/MockPieRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class MockPieRepository : IPieRepository
+    {
+        private readonly ICategoryRepository _categoryRepository = new MockCategoryRepository();
+        public IEnumerable<Pie> AllPies =>
+            new List<Pie>
+            {
+                new Pie{PieId = 1,Name="Strawberry Pie",Price=15.95M, ShortDescription="Lorem Ipsum",LongDescription="Blabla",Category = _categoryRepository.AllCategories.Where(x=>x.CategoryId == 1).FirstOrDefault()},
+                new Pie{PieId = 2,Name="Cheese cake",Price=18.95M, ShortDescription="Lorem Ipsum",LongDescription="Blabla",Category = _categoryRepository.AllCategories.Where(x=>x.CategoryId == 2).FirstOrDefault()},
+                new Pie{PieId = 3,Name="Rhubarb Pie",Price=15.95M, ShortDescription="Lorem Ipsum",LongDescription="Blabla",Category = _categoryRepository.AllCategories.Where(x=>x.CategoryId == 3).FirstOrDefault()},
+                new Pie{PieId = 3,Name="Pumpkin Pie",Price=12.95M, ShortDescription="Lorem Ipsum",LongDescription="Blabla",Category = _categoryRepository.AllCategories.Where(x=>x.CategoryId == 5).FirstOrDefault()}
+            };
+
+        public IEnumerable<Pie> PiesOfTheWeek { get; }
+
+        public Pie GetPieById(int pieId)
+        {
+            return AllPies.FirstOrDefault(p => p.PieId == pieId);
+        }
+    }
+}
